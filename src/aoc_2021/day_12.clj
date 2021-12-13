3f347febe3ec53e5ec32061fbdaa89eb039a6c10
mrgnu <mr.gnu.jr@gmail.com>
2021-12-13 14:04:56 +0100

    day 12: part 1

--- src/aoc_2021/day_12.clj
+++ src/aoc_2021/day_12.clj
@@ -1,16 +1,117 @@
 (ns aoc-2021.day-12
-  (:require [aoc-2021.utils :as utils])
+  (:require [aoc-2021.utils :as utils]
+            [clojure.set]
+            )
   )
 
 (def test-input-12-1
   [
+   "start-A"
+   "start-b"
+   "A-c"
+   "A-b"
+   "b-d"
+   "A-end"
+   "b-end"
+   ])
+
+(def test-input-12-2
+  [
+   "dc-end"
+   "HN-start"
+   "start-kj"
+   "dc-start"
+   "dc-HN"
+   "LN-dc"
+   "HN-end"
+   "kj-sa"
+   "kj-HN"
+   "kj-dc"
+   ])
+
+(def test-input-12-3
+  [
+   "fs-end"
+   "he-DX"
+   "fs-he"
+   "start-DX"
+   "pj-DX"
+   "end-zg"
+   "zg-sl"
+   "zg-pj"
+   "pj-he"
+   "RW-he"
+   "fs-DX"
+   "pj-RW"
+   "zg-RW"
+   "start-pj"
+   "he-WI"
+   "zg-he"
+   "pj-fs"
+   "start-RW"
    ])
 
 (defn input-12-1 []
   (->> "resources/day_12_1.txt"
        utils/per-line-input))
 
+(defn is-small? [n]
+  (let [s (name n)]
+    (= s (.toLowerCase s))))
+
+(defn- add-mapping [cave-map from to]
+  (update cave-map from conj to))
+
+(defn read-path-spec [line]
+  (let [[s & m] (re-find #"(\w+)-(\w+)" line)]
+    (assert (= 2 (count m)) "malformed path spec")
+    [(keyword (first  m))
+     (keyword (second m))]))
+
+(defn read-cave-map
+  "returns a map with mappings from cave to all adjacent"
+  [lines]
+  (->> lines
+       (map read-path-spec)
+       (reduce (fn [cave-map [a b]]
+                 (-> cave-map
+                     (add-mapping a b)
+                     (add-mapping b a)))
+               {})
+       ))
+
+(defn find-paths
+  "returns the set of valid paths from :start to :end"
+
+  ([cave-map] (find-paths cave-map #{} [] :start))
+
+  ([cave-map visited-small path cave]
+   (assert (not (contains? visited-small cave)))
+   (let [
+         ;; append currently visited cave to current path
+         path (conj path cave)
+         ;; update visited-small
+         visited-small (if (is-small? cave) (conj visited-small cave) visited-small)
+         ]
+     (if (= :end cave)
+       #{ path }
+       (let [to-visit (->> (cave cave-map)
+                           (remove
+                            (fn [c] (and (is-small? c)
+                                         (contains? visited-small c)))))]
+         (reduce (fn [paths c]
+                   (let [ps (find-paths cave-map visited-small path c)]
+                     (clojure.set/union paths ps)))
+                 #{}
+                 to-visit)))))
+  )
+
 (defn day-12-1 []
+  (->> (input-12-1)
+       read-cave-map
+       find-paths
+       count
+       )
   )
 
 (defn day-12-2 []
--- test/aoc_2021/day_12_test.clj
+++ test/aoc_2021/day_12_test.clj
@@ -5,6 +5,31 @@
 (deftest unit-test-day-12
   (testing "unit tests day 12"
     (testing "part 1"
+      (testing "example 1"
+        (let [cave-map (->> test-input-12-1 read-cave-map)
+              paths    (find-paths cave-map)]
+          (is (= 10 (count paths)))
+          (is (= #{
+                   [:start :A :b :A :c :A :end]
+                   [:start :A :b :A :end]
+                   [:start :A :b :end]
+                   [:start :A :c :A :b :A :end]
+                   [:start :A :c :A :b :end]
+                   [:start :A :c :A :end]
+                   [:start :A :end]
+                   [:start :b :A :c :A :end]
+                   [:start :b :A :end]
+                   [:start :b :end]
+                   }
+                 paths))
+          )
+        )
+      (testing "example 2"
+        (is (= 19 (->> test-input-12-2 read-cave-map find-paths count)))
+        )
+      (testing "example 3"
+        (is (= 226 (->> test-input-12-3 read-cave-map find-paths count)))
+        )
       )
 
     (testing "part 2"
@@ -14,6 +39,7 @@
 (deftest day-12
   (testing "day 12"
     (testing "part 1"
+      (is (= 4413 (day-12-1)))
       )
 
     (testing "part 2"
