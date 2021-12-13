ed735879485e40a2fe6e496351dd79a0eec79a4f
mrgnu <mr.gnu.jr@gmail.com>
2021-12-13 14:04:34 +0100

    day 3: part 2

--- src/aoc_2021/day_3.clj
+++ src/aoc_2021/day_3.clj
@@ -44,6 +44,20 @@
        (group-by identity)
        (map (fn [[k bits]] [k (count bits)]))))
 
+(defn- get-prominent-bit
+  [bit-row bit-selector-fn tie-selector-fn]
+  (let [bit-counts (get-bit-counts bit-row)]
+    (assert (= 2 (count bit-counts)) "bits are not bits")
+    (if (= (second (first bit-counts)) (second (second bit-counts)))
+      (tie-selector-fn bit-row)
+      (->> bit-counts
+           (sort-by second)
+           bit-selector-fn
+           first
+           )
+      ))
+   )
+
 (defn- most-prominent-bit
   "determines and returns the bit in a bit-row that has the most occurences.
   if 0s and 1s are equally represented, `tie-selector-fn' is called with bit-counts."
@@ -52,19 +66,17 @@
    )
 
   ([bit-row tie-selector-fn]
-  (let [bit-counts (get-bit-counts bit-row)]
-    (assert (= 2 (count bit-counts)) "bits are not bits")
-    (if (= (second (first bit-counts)) (second (second bit-counts)))
-      (tie-selector-fn bit-row)
-      (->> bit-counts
-           (sort-by second)
-           reverse
-           first
-           first
-           )
-      ))
+   (get-prominent-bit bit-row last tie-selector-fn)))
+
+(defn- least-prominent-bit
+  "determines and returns the bit in a bit-row that has the least occurences.
+  if 0s and 1s are equally represented, `tie-selector-fn' is called with bit-counts."
+  ([bit-row]
+   (most-prominent-bit bit-row (fn [_] (throw (AssertionError. "equal amount of 1s and 0s"))))
    )
-  )
+
+  ([bit-row tie-selector-fn]
+   (get-prominent-bit bit-row first tie-selector-fn)))
 
 (defn make-report
   "converts strings in input to vecs of ints"
@@ -75,10 +87,9 @@
     report))
 
 (defn get-gamma-rate [report bit-count]
-  (assert (validate-diagnostic-report report) "not a valid report")
   (let [bit-rows (map (partial get-bit-row report) (range bit-count))]
     (->> bit-rows
-         (map most-prominient-bit)
+         (map most-prominent-bit)
          from-bits
          )
     ))
@@ -105,5 +116,39 @@
        )
   )
 
+(defn- filter-by-bit [bit-rows bit-idx bit-value]
+  (filter (fn [bit-row] (= bit-value (nth bit-row bit-idx)))
+          bit-rows))
+
+(defn get-report-bit-field [report bit-count bit-selector-fn tie-selector-fn]
+    (loop [bit-idx 0
+           report  report]
+      (assert (< bit-idx bit-count) "no bits left")
+      (let [bit-row   (get-bit-row report bit-idx)
+            bit-value (bit-selector-fn bit-row tie-selector-fn)
+            relevant  (filter-by-bit report bit-idx bit-value)]
+        (assert (not-empty relevant) "no bit-rows left")
+        (if (= 1 (count relevant))
+          (first relevant)
+          (recur (inc bit-idx) relevant)))))
+
+(defn get-oxygen-generator-rating [bit-count report]
+  (let [oxy-bit (get-report-bit-field report bit-count most-prominent-bit (fn [_] 1))]
+    (from-bits oxy-bit)))
+
+(defn get-co2-scrubber-rating [bit-count report]
+  (let [co2-bit (get-report-bit-field report bit-count least-prominent-bit (fn [_] 0))]
+    (from-bits co2-bit)))
+
+(defn get-life-support-rating [report]
+  (let [bit-count ((comp count first) report)
+        oxygen-generator-rating (get-oxygen-generator-rating bit-count report)
+        co2-scrubber-rating (get-co2-scrubber-rating bit-count report)]
+    (* oxygen-generator-rating co2-scrubber-rating)))
+
 (defn day-3-2 []
+  (->> (input-3-1)
+       make-report
+       get-life-support-rating
+       )
   )
--- test/aoc_2021/day_3_test.clj
+++ test/aoc_2021/day_3_test.clj
@@ -2,22 +2,24 @@
   (:require [clojure.test :refer :all]
             [aoc-2021.day-3 :refer :all]))
 
-(deftest unit-test-day-2
+(deftest unit-test-day-3
   (testing "unit tests day 3"
     (testing "part 1"
       (is (= 198 (->> test-input-3-1 make-report get-power-consumption)))
       )
 
     (testing "part 2"
+      (is (= 230 (->> test-input-3-1 make-report get-life-support-rating)))
       )
     ))
 
-(deftest day-1
+(deftest day-3
   (testing "day 3"
     (testing "part 1"
       (is (= 2003336 (day-3-1)))
       )
 
     (testing "part 2"
+      (is (= 1877139 (day-3-2)))
       )
     ))
