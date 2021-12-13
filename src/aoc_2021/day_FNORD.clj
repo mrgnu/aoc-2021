88880d75c105683cf8029130c40a53266246b2b8
mrgnu <mr.gnu.jr@gmail.com>
2021-12-13 14:04:35 +0100

    day template

---
+++ src/aoc_2021/day_FNORD.clj
@@ -0,0 +1,17 @@
+(ns aoc-2021.day-FNORD
+  (:require [aoc-2021.utils :as utils])
+  )
+
+(def test-input-FNORD-1
+  [
+   ])
+
+(defn input-FNORD-1 []
+  (->> "resources/day_FNORD_1.txt"
+       utils/per-line-input))
+
+(defn day-FNORD-1 []
+  )
+
+(defn day-FNORD-2 []
+  )
---
+++ test/aoc_2021/day_FNORD_test.clj
@@ -0,0 +1,21 @@
+(ns aoc-2021.day-FNORD-test
+  (:require [clojure.test :refer :all]
+            [aoc-2021.day-FNORD :refer :all]))
+
+(deftest unit-test-day-FNORD
+  (testing "unit tests day FNORD"
+    (testing "part 1"
+      )
+
+    (testing "part 2"
+      )
+    ))
+
+(deftest day-FNORD
+  (testing "day FNORD"
+    (testing "part 1"
+      )
+
+    (testing "part 2"
+      )
+    ))
