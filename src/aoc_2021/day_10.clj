892a4ffa1461b361126a142573c27bede5670549
mrgnu <mr.gnu.jr@gmail.com>
2021-12-13 14:04:51 +0100

    day 10: from template

---
+++ src/aoc_2021/day_10.clj
@@ -0,0 +1,17 @@
+(ns aoc-2021.day-10
+  (:require [aoc-2021.utils :as utils])
+  )
+
+(def test-input-10-1
+  [
+   ])
+
+(defn input-10-1 []
+  (->> "resources/day_10_1.txt"
+       utils/per-line-input))
+
+(defn day-10-1 []
+  )
+
+(defn day-10-2 []
+  )
---
+++ test/aoc_2021/day_10_test.clj
@@ -0,0 +1,21 @@
+(ns aoc-2021.day-10-test
+  (:require [clojure.test :refer :all]
+            [aoc-2021.day-10 :refer :all]))
+
+(deftest unit-test-day-10
+  (testing "unit tests day 10"
+    (testing "part 1"
+      )
+
+    (testing "part 2"
+      )
+    ))
+
+(deftest day-10
+  (testing "day 10"
+    (testing "part 1"
+      )
+
+    (testing "part 2"
+      )
+    ))
