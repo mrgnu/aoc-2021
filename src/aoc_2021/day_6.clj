a0679d8db2c52085e88ff28d4fc21a4f01f53d53
mrgnu <mr.gnu.jr@gmail.com>
2021-12-13 14:04:41 +0100

    day 6: part 1

--- src/aoc_2021/day_6.clj
+++ src/aoc_2021/day_6.clj
@@ -3,14 +3,42 @@
   )
 
 (def test-input-6-1
-  [
-   ])
+  "3,4,3,1,2"
+  )
 
 (defn input-6-1 []
   (->> "resources/day_6_1.txt"
-       utils/per-line-input))
+       slurp
+       clojure.string/trim))
+
+(defn read-fish [s]
+  (as-> s x
+    (clojure.string/split x #",")
+      (map #(Integer. %) x)
+      )
+  )
+
+(defn count-fish-lifeline [^Integer days-to-count ^Integer days-until-spawn]
+  (if (<= days-to-count days-until-spawn)
+    1N
+    (let [days-left (- days-to-count days-until-spawn)]
+      (+ ^BigInteger (count-fish-lifeline days-left 9)
+         ^BigInteger (count-fish-lifeline days-left 7)))))
+
+(defn count-fish-lifelines-r [^Integer days-to-count ^ints fish]
+  (reduce (fn [acc days-until-spawn]
+            (+ acc (count-fish-lifeline days-to-count days-until-spawn)))
+          0
+          fish))
+
+(defn count-fish-lifelines [^Integer days-to-count ^ints fish]
+  (count-fish-lifelines-r days-to-count fish))
 
 (defn day-6-1 []
+  (->> (input-6-1)
+       read-fish
+       (count-fish-lifelines 80)
+   )
   )
 
 (defn day-6-2 []
--- test/aoc_2021/day_6_test.clj
+++ test/aoc_2021/day_6_test.clj
@@ -5,15 +5,37 @@
 (deftest unit-test-day-6
   (testing "unit tests day 6"
     (testing "part 1"
-      )
+      (is (= 26N (->> test-input-6-1 read-fish (count-fish-lifelines 18))))
+      (is (= 5934N (->> test-input-6-1 read-fish (count-fish-lifelines 80))))
 
-    (testing "part 2"
-      )
-    ))
+      (testing "count-fish-lifeline"
+        (is (= 1N (count-fish-lifeline 0 0)))
+
+        (is (= 2N (count-fish-lifeline 1 0)))
+        (is (= 2N (count-fish-lifeline 2 0)))
+        (is (= 2N (count-fish-lifeline 7 0)))
+
+        (is (= 3N (count-fish-lifeline 8 0)))
+        (is (= 3N (count-fish-lifeline 9 0)))
+
+        (is (= 4N (count-fish-lifeline 10 0)))
+        (is (= 4N (count-fish-lifeline 14 0)))
+
+        (is (= 5N (count-fish-lifeline 15 0)))
+        (is (= 5N (count-fish-lifeline 16 0)))
+
+        (is (= 7N (count-fish-lifeline 17 0)))
+        )
+    )
+
+  (testing "part 2"
+    )
+  ))
 
 (deftest day-6
   (testing "day 6"
     (testing "part 1"
+      (is (= 365131N (day-6-1)))
       )
 
     (testing "part 2"
