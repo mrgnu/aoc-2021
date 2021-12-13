508fee218585dcd281a6644ecbb113412ae971b4
mrgnu <mr.gnu.jr@gmail.com>
2021-12-13 14:04:45 +0100

    day 8: from template

---
+++ src/aoc_2021/day_8.clj
@@ -0,0 +1,17 @@
+(ns aoc-2021.day-8
+  (:require [aoc-2021.utils :as utils])
+  )
+
+(def test-input-8-1
+  [
+   ])
+
+(defn input-8-1 []
+  (->> "resources/day_8_1.txt"
+       utils/per-line-input))
+
+(defn day-8-1 []
+  )
+
+(defn day-8-2 []
+  )
---
+++ test/aoc_2021/day_8_test.clj
@@ -0,0 +1,21 @@
+(ns aoc-2021.day-8-test
+  (:require [clojure.test :refer :all]
+            [aoc-2021.day-8 :refer :all]))
+
+(deftest unit-test-day-8
+  (testing "unit tests day 8"
+    (testing "part 1"
+      )
+
+    (testing "part 2"
+      )
+    ))
+
+(deftest day-8
+  (testing "day 8"
+    (testing "part 1"
+      )
+
+    (testing "part 2"
+      )
+    ))
