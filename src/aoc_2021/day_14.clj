24baf007a7ae53030850c1660a49b5576fb378ca
mrgnu <mr.gnu.jr@gmail.com>
2021-12-14 09:22:01 +0100

    day 14: from template

---
+++ src/aoc_2021/day_14.clj
@@ -0,0 +1,17 @@
+(ns aoc-2021.day-14
+  (:require [aoc-2021.utils :as utils])
+  )
+
+(def test-input-14-1
+  [
+   ])
+
+(defn input-14-1 []
+  (->> "resources/day_14_1.txt"
+       utils/per-line-input))
+
+(defn day-14-1 []
+  )
+
+(defn day-14-2 []
+  )
