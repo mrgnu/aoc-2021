40750fd395533034d57925bd70fa6aa3200c294b
mrgnu <mr.gnu.jr@gmail.com>
2021-12-16 09:24:52 +0100

    day 16: from template

---
+++ src/aoc_2021/day_16.clj
@@ -0,0 +1,17 @@
+(ns aoc-2021.day-16
+  (:require [aoc-2021.utils :as utils])
+  )
+
+(def test-input-16-1
+  [
+   ])
+
+(defn input-16-1 []
+  (->> "resources/day_16_1.txt"
+       utils/per-line-input))
+
+(defn day-16-1 []
+  )
+
+(defn day-16-2 []
+  )
---
+++ test/aoc_2021/day_16_test.clj
@@ -0,0 +1,21 @@
+(ns aoc-2021.day-16-test
+  (:require [clojure.test :refer :all]
+            [aoc-2021.day-16 :refer :all]))
+
+(deftest unit-test-day-16
+  (testing "unit tests day 16"
+    (testing "part 1"
+      )
+
+    (testing "part 2"
+      )
+    ))
+
+(deftest day-16
+  (testing "day 16"
+    (testing "part 1"
+      )
+
+    (testing "part 2"
+      )
+    ))
