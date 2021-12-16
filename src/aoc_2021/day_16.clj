0372afec6bc7ff916dc187c8d181763d19fcb2cf
mrgnu <mr.gnu.jr@gmail.com>
2021-12-16 14:38:12 +0100

    day 16: part 1

--- src/aoc_2021/day_16.clj
+++ src/aoc_2021/day_16.clj
@@ -2,15 +2,154 @@
   (:require [aoc-2021.utils :as utils])
   )
 
-(def test-input-16-1
-  [
-   ])
+;; number: [v6 t4], 2021
+(def test-input-16-1 "D2FE28")
+
+;; operator: [v1 t6], len=27 bits, [lv 10, lv 20]
+(def test-input-16-2 "38006F45291200")
+
+;; operator: [v7 t3], n=3 packages, [lv 1, lv 2, lv 3]
+(def test-input-16-3 "EE00D40C823060")
 
 (defn input-16-1 []
   (->> "resources/day_16_1.txt"
-       utils/per-line-input))
+       slurp
+       clojure.string/trim
+       ))
+
+(defn- to-int [bits]
+  (reduce (fn [acc b]
+            (+ (* 2 acc) b))
+          0
+          bits))
+
+(defn- get-bits [^Integer i]
+  (reduce (fn [bits e]
+            (conj bits
+                  (if (zero? (bit-and (int (Math/pow 2 e)) i)) 0 1)))
+          []
+          (reverse (range 8))))
+
+(defn make-bit-holder [hex-str]
+  (assert (zero? (mod (count hex-str) 2)))
+  (let [bits
+        (reduce (fn [acc hex-pair]
+                  (let [bs (apply str "0x" hex-pair)
+                        i  (Integer/decode bs)]
+                    (into acc (get-bits i))))
+                []
+                (partition 2 hex-str))]
+    {
+     :bits   bits
+     :offset 0
+     :stream bits
+     }
+    ))
+
+(defn advance [n bit-holder]
+  (-> bit-holder
+      (update ,,, :stream (partial drop n))
+      (update ,,, :offset + n)
+      ))
+
+(defn eat [n {:keys [stream] :as bit-holder}]
+  (let [bits (take n stream)]
+    [bits
+     (advance n bit-holder)]))
+
+(defn read-value [n {:keys [stream] :as bit-holder}]
+  (let [[bits bit-holder] (eat n bit-holder)]
+    [(to-int bits)
+     bit-holder]))
+
+(defn read-header [bit-holder]
+  (let [[ver  bit-holder] (read-value 3 bit-holder)
+        [type bit-holder] (read-value 3 bit-holder)]
+    [{
+      :version ver
+      :type    type
+      }
+     bit-holder]))
+
+(defn read-literal-value [bit-holder]
+  (loop [bit-holder bit-holder
+         value-bits []]
+    (let [[prefix bit-holder] (read-value 1 bit-holder)
+          [chunk  bit-holder] (eat        4 bit-holder)]
+      (let [value-bits (into value-bits chunk)]
+        (if (zero? prefix)
+          [(to-int value-bits) bit-holder]
+          (recur bit-holder value-bits))))))
+
+(declare read-packet)
+
+(defn read-to-length [len bit-holder]
+  (let [start-offset (:offset bit-holder)
+        end-offset   (+ start-offset len)]
+    (loop [bit-holder bit-holder
+           packets    []]
+      (assert (<= (:offset bit-holder) end-offset) "read-to-length: too much read")
+      (if (= (:offset bit-holder) end-offset)
+        [packets bit-holder]
+        (let [[packet bit-holder] (read-packet bit-holder)
+              packets (conj packets packet)]
+          (recur bit-holder packets))))))
+
+(defn read-n [n bit-holder]
+  (reduce (fn [[packets bit-holder] _]
+            (let [[packet bit-holder] (read-packet bit-holder)
+                  packets (conj packets packet)]
+              [packets bit-holder]))
+          [[] bit-holder]
+          (range n)))
+
+(defn read-operator [bit-holder]
+  (let [[length-type-id bit-holder] (read-value 1 bit-holder)]
+    (if (zero? length-type-id)
+      ;; 15 bits - total length in bits
+      (let [[total-length bit-holder] (read-value 15 bit-holder)]
+        (read-to-length total-length bit-holder))
+      ;; 11 bits - number of sub-packets
+      (let [[sub-packet-count bit-holder] (read-value 11 bit-holder)]
+        (read-n sub-packet-count bit-holder)))))
+
+(defn read-packet
+  ([bit-holder] (apply read-packet (read-header bit-holder)))
+
+  ([header bit-holder]
+   (let [[packet bit-holder]
+         (condp = (:type header)
+           ;; 4: literal value
+           4 (read-literal-value bit-holder)
+           ;; else: operator
+           (read-operator bit-holder)
+           )]
+     [{
+       :header header
+       :packet packet
+       }
+      bit-holder]))
+  )
+
+(defn get-version-sum [packet]
+  (let [type    (get-in packet [:header :type])
+        version (get-in packet [:header :version])]
+    (condp = type
+      ;; 4: literal value
+      4 version
+      ;; else: operator
+      (let [sub-packages (:packet packet)]
+        (+ version
+           (apply + (map get-version-sum sub-packages))))
+      )))
 
 (defn day-16-1 []
+  (->> (input-16-1)
+       make-bit-holder
+       read-packet
+       first
+       get-version-sum
+       )
   )
 
 (defn day-16-2 []
--- test/aoc_2021/day_16_test.clj
+++ test/aoc_2021/day_16_test.clj
@@ -5,6 +5,13 @@
 (deftest unit-test-day-16
   (testing "unit tests day 16"
     (testing "part 1"
+      (testing "get-version-sum"
+        (let [sum-fn (fn [transmission]
+                       (->> transmission make-bit-holder read-packet first get-version-sum))]
+          (is (= 16 (sum-fn "8A004A801A8002F478")))
+          (is (= 23 (sum-fn "C0015000016115A2E0802F182340")))
+          (is (= 31 (sum-fn "A0016C880162017C3686B18A3D4780")))
+          ))
       )
 
     (testing "part 2"
@@ -14,6 +21,7 @@
 (deftest day-16
   (testing "day 16"
     (testing "part 1"
+      (is (= 960 (day-16-1)))
       )
 
     (testing "part 2"
