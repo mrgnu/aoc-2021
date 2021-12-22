0b0142140fa5df859738bb698303b71bf8f56917
mrgnu <mr.gnu.jr@gmail.com>
2021-12-22 15:43:57 +0100

    day 21: from template

---
+++ src/aoc_2021/day_21.clj
@@ -0,0 +1,17 @@
+(ns aoc-2021.day-21
+  (:require [aoc-2021.utils :as utils])
+  )
+
+(def test-input-21-1
+  [
+   ])
+
+(defn input-21-1 []
+  (->> "resources/day_21_1.txt"
+       utils/per-line-input))
+
+(defn day-21-1 []
+  )
+
+(defn day-21-2 []
+  )
---
+++ test/aoc_2021/day_21_test.clj
@@ -0,0 +1,21 @@
+(ns aoc-2021.day-21-test
+  (:require [clojure.test :refer :all]
+            [aoc-2021.day-21 :refer :all]))
+
+(deftest unit-test-day-21
+  (testing "unit tests day 21"
+    (testing "part 1"
+      )
+
+    (testing "part 2"
+      )
+    ))
+
+(deftest day-21
+  (testing "day 21"
+    (testing "part 1"
+      )
+
+    (testing "part 2"
+      )
+    ))
